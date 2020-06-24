dc3a04847abb9787ce6a74f157639385899ec90a
Howard Kim <howardkim820@gmail.com>
2020-06-23 18:47:22 -0700

    deploy

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-    apiKey: process.env.API_KEY,
-    mongoURI: process.env.MONGO_URI,
-    secretKey: process.env.SECRET_KEY
-}
+  apiKey: process.env.API_KEY,
+  mongoURI: process.env.MONGO_URI,
+  secret: process.env.SECRET_KEY,
+  domain: process.env.DOMAIN_KEY,
+};
