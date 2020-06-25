1118b3091b55afa47e2739018e82af22e350ffb3
Howard Kim <howardkim820@gmail.com>
2020-06-24 17:05:30 -0700

    change

--- client/src/components/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Spinner() {
-    return (
-      <div className="progress">
-        <div className="indeterminate"></div>
-      </div>
-    );
-}
--- middlewares/requireLogin.js
+++
@@ -1,7 +0,0 @@
-//
-module.exports = (req, res, next) => {
-  if (!req.user) {
-    return res.status(401).send({ error: "Please Log In!" });
-  }
-  next(); //lets let this user continue on to the request handler
-};
